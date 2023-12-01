b9fc71fe3e4e6184752bfc98f8e3f316c3e95902
ankitabhikhie7 <145680238+ankitabhikhie7@users.noreply.github.com>
2023-12-01 16:37:19 +0100

    Update H1O01.js

--- JS/H1/H1O01.js
+++ JS/H1/H1O01.js
@@ -1,32 +1,27 @@
 function setup() {
-  canvas = createCanvas(450,450);
-  background('darkred');
+  canvas = createCanvas(1000,450);
+  background('grey');
   canvas.parent('processing');
   noLoop();
 }
 
 function draw() {
-  // noStroke();
-  // fill('darkred');
-  // triangle(0,0,0,450,450,450);
-
-  strokeWeight(5);
-  stroke('pink');
-  fill('pink');
-  beginShape();
-  vertex(450,450);
-  vertex(1,225);
-  vertex(450,1);
-  endShape(CLOSE);
-
-  strokeWeight(5);
-  stroke('purple');
-  fill('purple');
-  beginShape();
-  vertex(1,1);
-  vertex(450,225);
-  vertex(1,450);
-  endShape(CLOSE);
-
+  // groene cirkel zonder rand
   
+  noStroke();
+  fill('green');
+  ellipse(225,220,390);
+  
+  // groene cirkel zonder rand
+  
+  noStroke();
+  fill('white');
+  ellipse(225,225,300);
+
+  // witte rechthoek met rode rand
+  
+  stroke('green');
+  fill('white');
+  strokeWeight(10);
+  rect(125,125,200,200);
 }
